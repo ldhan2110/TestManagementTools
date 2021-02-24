b62d5f73aa6415c81b473e4fcd0303e375496a8c
ldhan2110 <ldhan211099@gmail.com>
2021-02-24 11:27:01 +0700

    Update temp Milestone

--- front-end/src/pages/milstones/milestone-overview-page/index.js
+++ front-end/src/pages/milstones/milestone-overview-page/index.js
@@ -1,28 +1,106 @@
 import React from 'react';
-import { ProgressBar } from "react-milestone";
+import { makeStyles } from '@material-ui/core/styles';
+import Timeline from '@material-ui/lab/Timeline';
+import TimelineItem from '@material-ui/lab/TimelineItem';
+import TimelineSeparator from '@material-ui/lab/TimelineSeparator';
+import TimelineConnector from '@material-ui/lab/TimelineConnector';
+import TimelineContent from '@material-ui/lab/TimelineContent';
+import TimelineOppositeContent from '@material-ui/lab/TimelineOppositeContent';
+import TimelineDot from '@material-ui/lab/TimelineDot';
+import FastfoodIcon from '@material-ui/icons/Fastfood';
+import LaptopMacIcon from '@material-ui/icons/LaptopMac';
+import HotelIcon from '@material-ui/icons/Hotel';
+import RepeatIcon from '@material-ui/icons/Repeat';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 
-type Milestone = {
-  index: number,
-  size: number,
-  position: number,
-  current: boolean,
-  completed: boolean
-};
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    padding: '6px 16px',
+  },
+  secondaryTail: {
+    backgroundColor: theme.palette.secondary.main,
+  },
+}));
 
-const MileStonePage = (props) => {
-
-   
+export default function CustomizedTimeline() {
+  const classes = useStyles();
 
   return (
-    <ProgressBar 
-      percentage={30} 
-      milestoneCount={3}
-      Milestone={(milestone: Milestone) => <div>I am a milestone</div>}
-      CurrentMilestone={(milestone: Milestone) => <div>I am the current milestone</div>}
-      CompletedMilestone={(milestone: Milestone) => <div>I am a completed milestone</div>}
-      onMilestoneClick={milestoneIndex => {}} />
-
+    <Timeline align="alternate">
+      <TimelineItem>
+        <TimelineOppositeContent>
+          <Typography variant="body2" color="textSecondary">
+            23/10/2021
+          </Typography>
+        </TimelineOppositeContent>
+        <TimelineSeparator>
+          <TimelineDot>
+            <FastfoodIcon />
+          </TimelineDot>
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>
+          <Paper elevation={3} className={classes.paper}>
+            <Typography variant="h6" component="h1">
+              Eat
+            </Typography>
+            <Typography>Because you need strength</Typography>
+          </Paper>
+        </TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent>
+          <Typography variant="body2" color="textSecondary">
+            10:00 am
+          </Typography>
+        </TimelineOppositeContent>
+        <TimelineSeparator>
+          <TimelineDot color="primary">
+            <LaptopMacIcon />
+          </TimelineDot>
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>
+          <Paper elevation={3} className={classes.paper}>
+            <Typography variant="h6" component="h1">
+              Code
+            </Typography>
+            <Typography>Because it&apos;s awesome!</Typography>
+          </Paper>
+        </TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot color="primary" variant="outlined">
+            <HotelIcon />
+          </TimelineDot>
+          <TimelineConnector className={classes.secondaryTail} />
+        </TimelineSeparator>
+        <TimelineContent>
+          <Paper elevation={3} className={classes.paper}>
+            <Typography variant="h6" component="h1">
+              Sleep
+            </Typography>
+            <Typography>Because you need rest</Typography>
+          </Paper>
+        </TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot color="secondary">
+            <RepeatIcon />
+          </TimelineDot>
+        </TimelineSeparator>
+        <TimelineContent>
+          <Paper elevation={3} className={classes.paper}>
+            <Typography variant="h6" component="h1">
+              Repeat
+            </Typography>
+            <Typography>Because this is the life you love!</Typography>
+          </Paper>
+        </TimelineContent>
+      </TimelineItem>
+    </Timeline>
   );
 }
-
-export default (MileStonePage)
