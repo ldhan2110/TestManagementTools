b13bb22e6e882203250d5ac4179c58c3f60afced
Nguyễn Lê Cường <huenguyenht1962@gmail.com>
2021-07-14 22:23:50 +0700

    Update Insert image

--- front-end/src/components/markdown-input/insertImage.js
+++ front-end/src/components/markdown-input/insertImage.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Image } from 'cloudinary-react'
+import { Image, Transformation } from 'cloudinary-react';
 import axios from 'axios';
 import {
     Button,
@@ -8,16 +8,54 @@ import {
     DialogContent,
     DialogContentText,
     DialogTitle,
+    Typography,
   } from '@material-ui/core'
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Box from '@material-ui/core/Box';
+import PropTypes from 'prop-types';
+
+function CircularProgressWithLabel(props) {
+    return (
+      <Box position="relative" display="inline-flex" style={{marginLeft: 5}}>
+        <CircularProgress variant="determinate" {...props} />
+        <Box
+          top={0}
+          left={0}
+          bottom={0}
+          right={0}
+          position="absolute"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Typography variant="caption" component="div" color="textSecondary">{`${Math.round(
+            props.value,
+          )}%`}</Typography>
+        </Box>
+      </Box>
+    );
+  }
+
+  CircularProgressWithLabel.propTypes = {
+    /**
+     * The value of the progress indicator for the determinate variant.
+     * Value between 0 and 100.
+     */
+    value: PropTypes.number.isRequired,
+  };
 
 
 const InsertImage = (props) => {
 
     const {isOpen, insertImage, openMethod} = props;
 
+    const [loadProgress, setLoadProgress] = useState("");
+    const [loadEnable, setLoadEnable] = useState(false);
     const [open, setOpen] = useState(isOpen);
     const [imageSelected, setImageSelected] = useState("");
     const [uploadedImg, setUploadedImg] = useState("");
+    const [insertImg, setInsertImg] = useState("");
+    const [error, setError] = useState("");
 
     useEffect(()=>{
         setOpen(isOpen);
@@ -29,39 +67,58 @@ const InsertImage = (props) => {
         setImageSelected("");
     }
 
-    const uploadImage = () => {       
+    const uploadImage = () => {
+        setUploadedImg("");
+        setError("");
+        setLoadProgress(0);
+        setLoadEnable(true);    
         const files = imageSelected;
         if(files[0] !== undefined){
             const data = new FormData();
             data.append('file', files[0]);
             data.append('upload_preset', 'testcontrol');
             var temp = "";
-            axios.post("https://api.cloudinary.com/v1_1/testcontrol/image/upload", data)
-            .then(res => {setUploadedImg(res.data.secure_url)})
+            axios.post("https://api.cloudinary.com/v1_1/testcontrol/image/upload", data, config)
+            .then(res => {setUploadedImg(res.data.public_id); setInsertImg(res.data.secure_url); setLoadEnable(false); setLoadProgress(0)})
+            .catch(error => setError(error.response.data.error.message))
         }
+    }
+
+    const config = {
+        onUploadProgress: progressEvent => setLoadProgress(Math.round(progressEvent.loaded / progressEvent.total * 100))
     }
 
     return(
         <Dialog open={open} onClose={handleClose}>
             <DialogContent>
-                <DialogTitle>Insert image</DialogTitle>
+                <DialogTitle style={{marginLeft: -12, marginTop: -10}}>Insert image</DialogTitle>
                 <DialogContentText>
-                Choose an image to insert
+                    Choose an image to insert
                 </DialogContentText>
-                <input
+                <div style={{display: 'flex', justifyContent: 'flex-start', alignItems: 'center'}}>
+                    <input
                     accept="image/*"
                     id="contained-button-file"
                     multiple
                     type="file"
                     onChange={(event) => {setImageSelected(event.target.files)}}
-                />
-                <Button id="UpBtn" variant='outlined' color="primary" onClick={uploadImage}>Upload</Button>
-                {uploadedImg !== "" && <Image id="preview" cloudName="testcontrol" 
-                publicId={uploadedImg} style={{maxWidth: 500, maxHeight: 500}}/>}
+                    />
+                    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', marginLeft: 10, marginBottom: 10}} >              
+                        <Button id="UpBtn" variant='outlined' color="primary" onClick={uploadImage}>Upload</Button>
+                        {loadEnable &&<CircularProgressWithLabel value={loadProgress} />}
+                    </div>                
+                </div>
+                {error !== "" && <DialogContentText style={{color: 'red'}}>
+                {error}
+                </DialogContentText>}
+                {uploadedImg !== "" && 
+                <Image cloudName="testcontrol" publicId={uploadedImg} style={{maxHeight: 500, maxWidth: 500}} >
+                    <Transformation quality="auto" fetchFormat="auto" />
+                </Image>}
             </DialogContent>
 
             <DialogActions>
-                <Button id="InsertBtn" disabled={uploadedImg !== "" ? false : true} variant="contained" color="primary" onClick={()=>{insertImage(uploadedImg)}}>Insert</Button>
+                <Button id="InsertBtn" disabled={uploadedImg !== "" ? false : true} variant="contained" color="primary" onClick={()=>{insertImage(insertImg)}}>Insert</Button>
                 <Button onClick={handleClose} color="primary">
                     Cancel
                 </Button>
