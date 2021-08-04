10f37f6d1f7130810df69945faf74ca443776388
Nguyễn Lê Cường <huenguyenht1962@gmail.com>
2021-08-04 16:20:12 +0700

    Update search TC in exec

--- front-end/src/components/CheckboxTreeView/CheckboxTreeView.js
+++ front-end/src/components/CheckboxTreeView/CheckboxTreeView.js
@@ -4,25 +4,121 @@ import 'react-checkbox-tree/lib/react-checkbox-tree.css';
 
 const CheckboxTreeView = (props) => {
 
-   const {data, parentCallback, selected} = props;
+   const {data, parentCallback, selected, text, suite} = props;
 
    const [checked,setChecked] = useState(selected ? selected : []);
 
    const [expanded, setExpand] = useState([]);
+
+   const [ nodesFiltered, setNodesFiltered ] = React.useState([data]);
+
+   useEffect(()=>{
+    values = [];
+    filterTree();
+   },[text]);
+
+   useEffect(()=>{
+    values = [];
+    filterTreeSuite();
+   },[suite]);
 
    useEffect(()=>{
     parentCallback(checked);
    },[checked]);
 
    useEffect(()=>{
+     setNodesFiltered([data]);
     if (data){
         setExpand([data.value]);
     }
    },[data]);
+
+    const filterTree = () => {
+    // Reset nodes back to unfiltered state
+        if (!text || text == "" || text.length === 0) {
+    // Collapse all node when TEXT BOX empty
+            //setExpand([data.value]);
+            setNodesFiltered([data])
+            return;
+        }
+        const nodesFiltered = nodes => {
+            return nodes.reduce(filterNodes , [])  
+        };
+        setNodesFiltered(nodesFiltered([data]));
+        setExpand(values1);
+    }
+    const filterTreeSuite = () => {
+    // Reset nodes back to unfiltered state
+      if (!suite || suite == "" || suite.length === 0) {
+    // Collapse all node when SUITE BOX empty
+          setExpand([data.value]);
+          setNodesFiltered([data])
+          return;
+      }
+      const nodesFiltered = nodes => {
+          return nodes.reduce(filterNodesSuite , [])  
+      };
+      setNodesFiltered(nodesFiltered([data]));
+    // Auto expand to filtered node (may cause lag)
+      setExpand(values1);
+  }
+    // Filter nodes on TEXT box
+    const filterNodes = (filtered, node) => {
+        const children = (node.children || []).reduce(filterNodes, []);
+        if (
+        // Node's label matches the search string
+            node.labelNoStyle.toLocaleLowerCase().indexOf(text.toLocaleLowerCase()) > -1 ||
+        // Or a children has a matching node
+            children.length
+        ) {
+            filtered.push({...node, ...children.length && {children}});
+            values1 = getAllValuesFromNodes(filtered, true, node.value);
+        }
+        return filtered;
+    }
+
+    // Filter nodes on SUITE box
+    const filterNodesSuite = (filtered, node) => {
+      const children = (node.children || []).reduce(filterNodesSuite, []);
+      if (
+      // Node's label matches the search string
+          node.labelNoStyle.toLocaleLowerCase().indexOf(suite.toLocaleLowerCase()) === 0 ||
+      // Or a children has a matching node
+          children.length
+      ) {
+          filtered.push({...node, ...children.length && {children}});
+          values1 = getAllValuesFromNodes(filtered, true, node.value);
+      }
+      return filtered;
+  }
+    // Temp arrays for storing expand nodes after filter
+    var values = [];
+    var values1 = [];
+    // Get all values up till searched nodes to expand
+    const getAllValuesFromNodes = (nodes, firstLevel, val) => {      
+      if (firstLevel) {
+        for (let n of nodes) {
+          values.push(n.value);
+          if(n.value === val){ return values; }          
+          if (n.children) {
+            values.push(getAllValuesFromNodes(n.children, false, val));
+          }
+        }
+      } else {
+        for (let n of nodes) {
+          if(n.value === val){ return values }
+          values.push(n.value);
+          if (n.children) {
+            values.push(getAllValuesFromNodes(n.children, false, val));
+          }
+        }
+      }
+    };
+
    
     return (
             <CheckboxTree
-                nodes={[data]}
+                nodes={text.length !== 0 ? nodesFiltered : [data]}
                 checked={checked}
                 expanded={expanded}
                 onCheck={checked => setChecked( checked )}
--- front-end/src/pages/testcases/select-test-case-page/index.js
+++ front-end/src/pages/testcases/select-test-case-page/index.js
@@ -11,11 +11,124 @@ import {
   DialogContent,
   DialogTitle,
   Button,
-  Grid,
-  AppBar, Toolbar, IconButton, Typography
+  Grid, Popper,
+  AppBar, Toolbar, IconButton, Typography,
 } from '@material-ui/core'
 import CircularProgress from '@material-ui/core/CircularProgress';
 import CloseIcon from '@material-ui/icons/Close';
+
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import SearchIcon from '@material-ui/icons/Search';
+import InputBase from '@material-ui/core/InputBase';
+import { fade, makeStyles, withStyles } from '@material-ui/core/styles';
+import Autocomplete, { createFilterOptions } from '@material-ui/lab/Autocomplete';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    //flexGrow: 1,
+    borderRadius: 4, //theme.shape.borderRadius
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(5),
+      width: 'auto',
+    },
+     display: 'flex',
+     flexDirection: 'row',
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+  },
+  search: {
+    search: {
+      position: 'relative',
+      borderRadius: theme.shape.borderRadius,
+      marginLeft: 0,
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(1),
+        width: 'auto',
+      },
+    },   
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(2, 2, 2, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1.1em + ${theme.spacing(5)}px)`,
+    fontSize: '16px',
+    transition: theme.transitions.create('width'),
+    width: '100%',
+   /* [theme.breakpoints.up('sm')]: {
+      width: '22ch',
+       '&:focus': {
+        width: '20ch',
+      }, 
+    },*/
+  },
+  margin: {
+    margin: theme.spacing(0),
+  },
+  divSearch: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+}));
+
+const BootstrapInput = withStyles((theme) => ({
+  root: {
+    marginLeft: 5,
+    borderRadius: '0px 4px 4px 0px',
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    'label + &': {
+      marginTop: theme.spacing(0),
+    },
+  },
+  input: {    
+    position: 'relative',    
+    border: '0px solid #ced4da',
+    fontSize: '16px',
+    padding: theme.spacing(2, 2, 2, 2),
+    marginLeft: 0,
+    width: '140px',
+    color: 'white',
+    transition: theme.transitions.create(['border-color', 'box-shadow']),
+    // Use the system font instead of the default Roboto font.
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+    '&:focus': {
+      borderRadius: '0px 4px 4px 0px',
+      borderColor: '#80bdff',
+      boxShadow: '0 0 0 0.1rem rgba(0,123,255,.25)',
+    },
+  },
+}))(InputBase);
 
 
 //MAP STATES TO PROPS - REDUX
@@ -23,7 +136,7 @@ const  mapStateToProps = (state) => {
   return { 
     project: state.project.currentSelectedProject,
     testcase: state.testcase,
-    listtestcaseselect: state.testcase.listTestcaseSelect,
+    listtestcaseselect: state.testcase.listTestcaseSelect
    }
 }
 
@@ -35,12 +148,12 @@ const mapDispatchToProps = dispatch => {
     getAllTestsuiteReq: (payload) => dispatch({type: GET_ALL_TESTSUITE_REQ,payload}),
     getListTestcaseSelectReq: (payload) => dispatch({type: GET_LIST_TESTCASE_SELECT_REQ, payload}),
     getAllTestsuiteNoTreeReq: (payload) => dispatch({type: GET_ALL_TESTSUITE_NO_TREE_REQ,payload})
-
   }
 }
 
 
 const SelectTestCasePopup = (props) => {
+  const classes = useStyles();
   
   const { getAllTestcaseReq, testcase, project, getListTestcaseSelectReq, selected, getAllTestsuiteReq, getAllTestsuiteNoTreeReq} = props;
   
@@ -58,7 +171,10 @@ const SelectTestCasePopup = (props) => {
       return result;
   }
 
-  const [data,setData] = useState(convertData(selected));
+  const [data, setData] = useState(convertData(selected));
+
+  //const [pressSearch, setPressSearch] = useState(false);
+  
 
   const handleClose = () =>{
       setOpen(false);
@@ -66,22 +182,38 @@ const SelectTestCasePopup = (props) => {
 
   const handleSelect = (Data) =>{
     setData(Data)
-}
+  }
 
-const handleSelectTestcase = () =>{
-  getListTestcaseSelectReq(data);
-  setOpen(false);
-}
-
+  const handleSelectTestcase = () =>{
+    getListTestcaseSelectReq(data);
+    setOpen(false);
+  }
+  
   useEffect(()=>{
       setOpenPopup(isOpen);
   },[isOpen, open])
 
   useEffect(()=>{
-    setListTestCase(testcase.listTestsuiteNoTree);
+    if(testcase.listTestsuiteNoTree)
+      setListTestCase(build()[0]);
   },[testcase.listTestsuiteNoTree])
 
   useEffect(()=>{
+    if(testcase.successNoTree === true) {
+      [testcase.listTestsuiteNoTree].filter(item => iterateObject(item));
+      setListTC(arr);
+    }
+  },[testcase.successNoTree])
+
+  // useEffect(()=>{
+  //   if(pressSearch === true && testcase.searchInExecSuccess === true) {
+  //     setListTestCase(testcase.listTestcaseInExec);
+  //     setPressSearch(false);
+  //   }
+  // },[testcase.searchInExecSuccess])
+
+  useEffect(()=>{
+    testcase.searchInExecSuccess = null;
     testcase.successNoTree = null;
     getAllTestsuiteReq(project);
     getAllTestcaseReq(project); 
@@ -90,6 +222,193 @@ const handleSelectTestcase = () =>{
 
   const theme = useTheme();
 
+  const [search, setSearch] = useState({
+    testcasename: '',
+    testsuite: '',
+    priority: ''
+  });
+
+  const [listTC, setListTC] = useState([]);
+  const [inputVal, setInputVal] = useState("");
+  const [val, setVal] = useState("");
+  let arr = [];
+  function iterateObject(obj, value) {
+    for(var prop in obj) {      
+      if(typeof obj[prop] === "object"){
+        iterateObject(obj[prop], value);
+      } else {
+        if(prop === 'label' && obj[prop] === value && value) {
+          for(var i = 0; i < obj['children']?.length; i++){
+            arr.push(obj['children'][i].label);
+          }          
+        }
+        else if(value === undefined)
+        if(obj["type"] === "TC" && prop === "label"){
+          arr.push(obj[prop]);
+        }
+      }
+    }
+  }
+
+  const handleChangeSuite = (event) => {
+    setSearch({...search, testsuite: event.target.value});
+    arr = [];
+    if(event.target.value !== ""){
+      [testcase.listTestsuiteNoTree].filter(item => iterateObject(item, event.target.value));
+      setListTC(arr);
+    } else {
+      [testcase.listTestsuiteNoTree].filter(item => iterateObject(item));
+      setListTC(arr);
+    }
+  }
+  const PopperMy = function (props) {
+    return (<Popper {...props} style={{ width: '355px' }} placement='bottom-start' />)
+  }
+
+  //----------------------------------------------------------------------------------------
+
+  // Format node that has no child
+  const formatDeepestChild = (node) => {
+    var formattedDChild = {};
+      // custom node properties when Test suite empty
+      var customLabel = node.type === 'TS' ? node.label + " (Empty)" : node.label
+      var customIconLeaf = node.type === 'TS' ? <span className="rct-icon rct-icon-parent-close" /> : null
+      var expandable = node.type === 'TS' ? true : false;
+      //var checkbox = node.type === 'TC' ? true : false;
+      var eleAssigned = node.is_assigned;
+      // coloring leaf node  
+      if(eleAssigned === true){
+      var dChildObj = {
+        value: node.value,
+        isLeaf: true,
+        icon: customIconLeaf,
+        label: <span style={{ color: "red" }}>{customLabel}</span>,
+        labelNoStyle: node.label,
+        is_assigned: eleAssigned,
+        //showCheckbox: checkbox,
+        disabled: expandable,
+      };
+      formattedDChild = dChildObj;
+    } else {  // no coloring
+      var dChildObj = {
+        value: node.value,
+        isLeaf: true,
+        icon: customIconLeaf,
+        label: customLabel,
+        labelNoStyle: node.label,
+        is_assigned: eleAssigned,
+        //showCheckbox: checkbox,
+        disabled: expandable,
+      };
+      formattedDChild = dChildObj;
+    }    
+    return formattedDChild;
+  }
+  // Format node
+  const formatChild = (node) => {
+    var formatedChild = {};
+    let arr = [];
+
+    // if no child
+    if(node.children === undefined){
+      var tempFor = formatDeepestChild(node);
+      return tempFor
+    }
+    // if has child
+    if(node.children) {
+      node.children.forEach(elem =>{      
+      arr.push(formatChild(elem));     
+      }
+    )}
+      // format suite and coloring
+      //let type = node.type;
+      let asgn = node.is_assigned;
+      if(asgn === true){
+        let childNode = {
+          value: node.value,
+          label: <span style={{ color: "red" }}>{node.label}</span>,
+          labelNoStyle: node.label,
+          is_assigned: asgn,
+          isLeaf: true,
+          parentId: node.label,        
+          children: arr,
+        }
+        formatedChild = childNode;
+      } else {
+        let childNode = {
+          value: node.value,
+          label: node.label,
+          labelNoStyle: node.label,
+          is_assigned: asgn,
+          isLeaf: true,
+          parentId: node.label,        
+          children: arr,
+        }
+        formatedChild = childNode;
+      }
+    return formatedChild;
+    
+  }    
+  // Format root node
+  const formatData = () => {
+    var formattedData = [];
+    
+      var element = testcase.listTestsuiteNoTree;
+      var rootPath = element.label;
+      var rootVal = element.value;
+
+      // if root has child
+      if(element?.children?.length > 0) {
+        var tempCh = [];
+        // push formated child to tempCh
+        element.children.forEach(ele => {          
+          tempCh.push(formatChild(ele))
+        });
+        // format root with child
+        var obj = {
+          value: rootVal,
+          parentId: "",
+          label: rootPath,
+          labelNoStyle: rootPath,
+          children: tempCh
+        }
+        formattedData.push(obj);
+      } else {  // root has no child
+        var obj = {
+          value: rootVal,
+          parentId: "",
+          label: rootPath,
+          labelNoStyle: rootPath,
+          icon: <span className="rct-icon rct-icon-parent-close" />,
+          showCheckbox: false,
+          children: []
+        }
+        formattedData.push(obj);
+      }
+    return formattedData;
+  }
+  // Make node map to render in checkbox treeview
+  const build = () => {
+    var data = formatData();
+    var nodeMap = {};
+    data.forEach(item => {      
+      nodeMap[item.value] = item;
+    }); 
+  
+    var finalArr = [];
+  
+    Object.keys(nodeMap).forEach(item => {
+      var parentId = nodeMap[item].parentId;
+      if(parentId){
+        
+        nodeMap[nodeMap[item].parentId].children.push(nodeMap[item])
+      }
+      else{
+          finalArr.push(nodeMap[item]);
+      }
+    });
+    return finalArr;
+  }
 
   return (
     <React.Fragment > 
@@ -105,23 +424,86 @@ const handleSelectTestcase = () =>{
             <Typography variant="h6">
               Select Test Case
             </Typography>
-           
+
+            {/* Search Test Case */}          
+            <div className={classes.root}>           
+            
+            <div className={classes.search}>            
+              <Autocomplete
+                value={val}
+                onChange={(event, newValue) => {
+                  setVal(newValue);        
+                }}
+                inputValue={inputVal}
+                onInputChange={(event, newInputValue) => {
+                  setInputVal(newInputValue);
+                }}
+                selectOnFocus
+                handleHomeEndKeys
+                id="free-solo-with-text-demo"
+                options={listTC}
+                renderOption={(option) => option}
+                style={{ width: '350px' }}
+                freeSolo
+                autoComplete
+                PopperComponent={PopperMy}
+                renderInput={(params) => {
+                  const {InputLabelProps,InputProps,...rest} = params;
+                  return(
+                  <div className={classes.divSearch}>
+                    <div className={classes.searchIcon}>
+                      <SearchIcon />
+                    </div>
+                    <InputBase
+                      {...params.InputProps} {...rest}                
+                      placeholder="Search test case…"
+                      classes={{
+                        root: classes.inputRoot,
+                        input: classes.inputInput,
+                      }} 
+                    />
+                  </div>
+                  )}
+                }
+              />      
+            </div>
+
+            <FormControl className={classes.margin}>
+              <Select
+                labelId="demo-customized-select-label"
+                id="demo-customized-select"
+                value={search.testsuite}
+                displayEmpty
+                onChange={handleChangeSuite}
+                input={<BootstrapInput />}
+              >
+              <MenuItem value="">
+                <em style={{opacity: 0.9}}>Any test suite</em>
+              </MenuItem>
+              {testcase.listTestsuite.map((item) => (
+                <MenuItem value={item.name}>{item.name}</MenuItem>
+              ))}
+              </Select>
+            </FormControl>
+            </div>
+            {/* Search Test Case */}
+
           </Toolbar>
         </AppBar>
         <DialogContent dividers>
-        {testcase.successNoTree === "" ? 
+        {(testcase.successNoTree === "" || testcase.searchInExecSuccess === "") ? 
           <div style={{height:'100%', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
             <CircularProgress />
           </div> :     
-          <Grid container spacing={1} style={{height: '30vh',maxHeight: '30vh', width: '20vw', maxWidth:'20vw'}}>          
+          <Grid container spacing={1} style={{height: '30vh',maxHeight: '30vh', width: '25vw', maxWidth:'30vw', padding:7}}>          
             <Grid item xs={12}>              
-              <CheckboxTreeView data={testcase.listTestsuiteNoTree} parentCallback={handleSelect} selected={data}/>
+              <CheckboxTreeView data={listTestCase} text={inputVal} suite={search.testsuite} parentCallback={handleSelect} selected={data}/>
             </Grid>
           </Grid>}
         </DialogContent>
 
         <DialogActions>
-          <Button color="primary" disabled={testcase.successNoTree === ""? true:false} onClick={handleSelectTestcase}>
+          <Button color="primary" disabled={testcase.successNoTree === "" ? true : false} onClick={handleSelectTestcase}>
             Select
           </Button>
         </DialogActions>
